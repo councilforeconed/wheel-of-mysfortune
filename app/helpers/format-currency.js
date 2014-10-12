cb8efe6ef2c069103e524d34a12135465e555fb5
Steve Kinney <hello@stevekinney.net>
2014-10-11 23:10:06 -0600

    Scorch the earth

--- app/helpers/format-currency.js
+++
@@ -1,3 +0,0 @@
-export default Ember.Handlebars.makeBoundHelper(function(number) {
-  return numeral(number).format('$0,0.00');
-});
--- app/helpers/format-percent.js
+++
@@ -1,6 +0,0 @@
-/* global numeral */
-
-export default Ember.Handlebars.makeBoundHelper(function(number) {
-  if (number.toString() === number) return number;
-  return numeral(number).format('0.00%');
-});
